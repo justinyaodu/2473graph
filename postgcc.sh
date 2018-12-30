d7778e23d7f913bbdbe1b0d392c6d97f433cda41
Justin Yao Du <justinyaodu@gmail.com>
2018-12-30 12:33:56 -0800

    added note functionality, added notes to existing items, added some chassis frame work items

--- postgcc.sh
+++ postgcc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # replace fictional progress attribute with fill color
-sed -i -r -e 's/progress=([0-9.]+)/fillcolor="gray;\1:white"/g' $1.gv
+# sed -i -r -e 's/progress=([0-9.]+)/fillcolor="gray;\1:white"/g' $1.gv
